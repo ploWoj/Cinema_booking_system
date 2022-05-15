45c8d291dbed97bf6da58143332be5ea4a1b255b
Wojtek <wojtek.plociennik@gmail.com>
2022-05-15 21:29:20 +0200

    Implementing function readfromfile

--- seat.py
+++ seat.py
@@ -1,10 +1,16 @@
+import os
+
 class Seat:
 
-    def __init__(self, name, surname, seatID, reserved):
+    PolishMenPopularName = []
+    PolishWomenPopularName = []
+
+    def __init__(self, name, surname, seatID, reserved, lista):
         self.name = name
         self.surname = surname
         self.seatID = seatID
         self.reserved = reserved
+        self.PolishMenPopularName = lista
     
     def isReserved(self):
         return self.reserved
@@ -13,7 +19,10 @@ class Seat:
         if self.isReserved():
             print("The place is already booked\n")
         else:
-            self.name = input("Please give me your name: ")
+            name = input("Please give me your name: ")
+            # if not name.isalpha:
+            #     raise ValueError("Given name has to include only letters")
+
             self.surname = input("Please give me your name: ")
             self.seatID = input("Please give a seat number")
             self.isReserved = True
@@ -34,3 +43,23 @@ class Seat:
         else:
             print("Seat number {} has been free.".format(self.seatID))
     
+
+#  napisz funkcje ktora zszczytuje z pluku imiona i inicjalizuje zmienna imiona meskie
+    @classmethod
+    def loadMenNameFromFile(cls, path):
+        
+        lista = []
+        with open(path, 'r') as f:
+            for line in f:
+                line = line.replace('\n','')
+                lista.append(line)
+        return lista
+        
+
+
+seat = Seat('Wojciech', 'Plociennik', 32, True, [])
+
+
+lista = Seat.PolishMenPopularName
+
+print(lista)
