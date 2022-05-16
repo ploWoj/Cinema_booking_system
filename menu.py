9851d053dd868051eafe1f4e81d7e3c6e25feb41
Wojtek <wojtek.plociennik@gmail.com>
2022-05-16 17:26:54 +0200

    correct exceptions

--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from os import system
 from cinemaHall import CinemaHall
 from cinema import Cinema
@@ -19,38 +20,49 @@ class Menu:
     def selectMovie(self):
         system("clear")
         self.cinema.displayAllFilms()
-        number = int(input("\nWhat film you want to choose\n"))
-
+        number = self.validIntiger()
+        
         return self.cinema.listOfHalls[number]
+            
         
     def runMenu(self):
         while True:
             
-
             self.printMenu()
 
-            choice = int(input("Select an option: "))
+            try:
+                choice  = self.validIntiger()
+                if choice == 1:
+                    selectedHall = self.selectMovie()
+                    selectedHall.addReservation()
 
-            if choice == 1:
-                
-                selectedHall = self.selectMovie()
-                selectedHall.addReservation()
-            elif choice == 2:
-                selectedHall = self.selectMovie()
-                selectedHall.cancelReservation()
-            elif choice == 3:
-                selectedHall = self.selectMovie()
-                selectedHall.checkSeat()
-            elif choice == 4:
-                selectedHall = self.selectMovie()
-                selectedHall.displayAllPlaces()
+                elif choice == 2:
+                    selectedHall = self.selectMovie()
+                    selectedHall.cancelReservation()
 
-            elif choice == 5:
-                selectedHall = self.selectMovie()
-                selectedHall.displayAvailableSeats()
+                elif choice == 3:
+                    selectedHall = self.selectMovie()
+                    selectedHall.checkSeat()
 
-            elif choice == 6:
-                selectedHall = self.selectMovie()
-                selectedHall.displayReservedSeats()
-            else:
-                exit()
+                elif choice == 4:
+                    selectedHall = self.selectMovie()
+                    selectedHall.displayAllPlaces()
+
+                elif choice == 5:
+                    selectedHall = self.selectMovie()
+                    selectedHall.displayAvailableSeats()
+
+                elif choice == 6:
+                    selectedHall = self.selectMovie()
+                    selectedHall.displayReservedSeats()
+                else:
+                    exit()
+            except ValueError as e:
+                print("Wrogng type entered!", e, '\n')
+
+
+    def validIntiger(self):
+        number = int(input("\nEnter a number\n"))
+        if type(number) != int :
+            raise ValueError("Entered type is not intiger!")
+        return number
